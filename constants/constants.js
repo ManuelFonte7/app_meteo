3d2804c461898407b02f12f6ef51f66f11a4abe3
ManuelFonte7 <manubusiness7@gmail.com>
2024-10-29 15:35:32 +0100

    Applicazione finita

---
+++ constants/constants.js
@@ -0,0 +1,19 @@
+export const apiKey = '65d01d42c44c499abae175720242810';
+
+export const weatherImages = {
+    'Sunny': require('../assets/sunny.png'),
+    'Heavy rain': require('../assets/heavyrain.png'),
+    'Moderate or heavy rain with thunder': require('../assets/temporale.png'),
+    'Overcast': require('../assets/cloudy.png'),
+    'Light rain': require('../assets/lightRain.png'),
+    'Clear': require('../assets/sunny.png'),
+    'Cloudy': require('../assets/nuvoloso.png'),
+    'Moderate rain': require('../assets/moderateRain.png'),
+    'Patchy rain possible': require('../assets/patchyRainPossibly.png'),
+    'Moderate rain at times': require('../assets/patchyRainPossibly.png'),
+    'Heavy rain at times': require('../assets/heavyrain.png'),
+    'Moderate or heavy freezing rain': require('../assets/freezingRain.png'),
+    'Moderate or heavy rain shower': require('../assets/heavyrain.png'),
+    'Partly cloudy': require('../assets/partlyCloudy.png'),
+    'other': require('../assets/nuvoloso.png')
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,31 +1,81 @@
-import React, {useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import { View, Image, SafeAreaView, TextInput, TouchableOpacity, Text, ScrollView } from "react-native";
 import { StatusBar } from 'expo-status-bar';
 import {MagnifyingGlassIcon} from 'react-native-heroicons/outline';
 import {MapPinIcon, CalendarDaysIcon} from 'react-native-heroicons/solid';
-
+import {debounce} from 'lodash';
+import { fetchLocations, fetchWeatherForecast } from '../api/weather';
+import { weatherImages } from '../constants/constants';
+import * as Progress from 'react-native-progress';
+import { getData, storeData } from '../utils/asyncStorage';
 
 
 export default function HomeScreen(){
     const [showSearch, toggleSearch] = useState(false);
-    const [locations, setLocations] = useState([1,2,3]);
-
+    const [locations, setLocations] = useState([]);
+    const [weather, setWeather] = useState({});
+    const [loading, setLoading] = useState(true);
+    
     const handleLocation = (loc) => {
-        console.log('Luogo: ', loc);
+        setLocations([]);
+        toggleSearch(false);
+        setLoading(true);
+        fetchWeatherForecast({
+            cityName: loc.name,
+            days: '7'
+        }).then(data => {
+            setWeather(data);
+            setLoading(false);
+            storeData('city', loc.name);
+        })
+    };
+
+    const handleSearch = value => {
+        if(value.length>2){
+            fetchLocations({cityName: value}).then(data =>{
+                setLocations(data);
+            })
+        }
+    };
+
+    useEffect(() => {
+        fetchMyWeatherData();
+    }, []);
+
+    const fetchMyWeatherData = async () => {
+        let myCity = await getData('city');
+        let cityName = 'Rome';
+        if(myCity) cityName = myCity;
+        fetchWeatherForecast({
+            cityName,
+            days: '7'
+        }).then(data => {
+            setWeather(data);
+            setLoading(false);
+        })
     }
+    const handleTextDebounce = useCallback(debounce(handleSearch, 1200), []);
+
+    const {current, location} = weather;
 
     return(
         <View style={{flex: 1, position: 'relative'}}>
             <StatusBar style='light' />
             <Image source={require('../assets/bg.jpg')} blurRadius={70} style={{position: 'absolute', height: '100%', width: '100%'}} />
-
-            <SafeAreaView style={{display: 'flex', flex: 1}}>
+            {
+                loading? (
+                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                        <Progress.CircleSnail thickness={10} size={140} color="#0bb3b2" />
+                    </View>
+                ):(
+                    <SafeAreaView style={{display: 'flex', flex: 1}}>
                 {/* sezione ricerca */}
                 <View style={{height: '7%', marginLeft: 4, marginRight: 4, position: 'relative', zIndex: 50}}>
                     <View style={{flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center', borderRadius: 9999, backgroundColor: showSearch? 'rgba(255, 255,255, 0.2)': 'transparent'}}>
                         {
                             showSearch? (
-                                <TextInput 
+                                <TextInput
+                                    onChangeText={handleTextDebounce}
                                     placeholder='Cerca una città' 
                                     placeholderTextColor={'lightgray'} 
                                     style={{paddingLeft: 24, height: 48, flex: 1,  fontSize: 16, lineHeight: 24, color: 'white', paddingBottom: 4}}/>
@@ -50,7 +100,7 @@ export default function HomeScreen(){
                                                     onPress={() => handleLocation(loc)}>
 
                                                     <MapPinIcon size={20} color={'gray'}/>
-                                                    <Text style={{color: 'black', fontSize: 18, lineHeight: 18, marginLeft: 8}}>London, United Kingdom</Text>
+                                                    <Text style={{color: 'black', fontSize: 18, lineHeight: 18, marginLeft: 8}}>{loc?.name}, {loc?.country}</Text>
                                                 </TouchableOpacity>
                                             )
                                         })
@@ -65,34 +115,34 @@ export default function HomeScreen(){
 
                         {/* Luoghi */}
                         <Text style={{color: 'white', textAlign: 'center', fontSize: 24, lineHeight: 32, fontWeight: 'bold'}}>
-                            London,
-                            <Text style={{fontWeight: 'semibold', fontSize: 18, lineHeight: 28, color: 'gray'}}>United Kingdom</Text>
+                            {location?.name}, 
+                            <Text style={{fontWeight: 'semibold', fontSize: 18, lineHeight: 28, color: 'gray'}}>{" " + location?.country}</Text>
                         </Text>
 
                          {/* Immagine meteo */}
                          <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                            <Image source={require('../assets/nuvoloso.png')} style={{width: 208, height: 208}}/>
+                            <Image source={weatherImages[current?.condition?.text]} style={{width: 208, height: 208}}/>
                          </View>
 
                          {/* Gradi */}
                          <View style={{marginTop: 8}}>
-                            <Text style={{textAlign: 'center', fontWeight: 'bold', color: 'white', fontSize: 60, marginLeft: 20}}>23&#176;</Text>
-                            <Text style={{textAlign: 'center', color: 'white', lineHeight: 28, fontSize: 20, marginLeft: 20, letterSpacing: 0.1}}>Parzialmente nuvoloso</Text>
+                            <Text style={{textAlign: 'center', fontWeight: 'bold', color: 'white', fontSize: 60, marginLeft: 20}}>{current?.temp_c}&#176;</Text>
+                            <Text style={{textAlign: 'center', color: 'white', lineHeight: 28, fontSize: 20, marginLeft: 20, letterSpacing: 0.1}}>{current?.condition?.text}</Text>
                          </View>
 
                          {/* Altri dati */}
                          <View style={{flexDirection: 'row', justifyContent: 'space-between', marginLeft: 16, marginRight: 16}}>
                             <View style={{flexDirection: 'row', marginLeft: 2, alignItems: 'center'}}>
                                 <Image source={require('../assets/wind.png')} style={{height: 28, width: 28}}/>
-                                <Text style={{color: 'white', fontWeight: 'semibold', fontSize: 16, lineHeight: 24, marginLeft: 6}}>22 km</Text>
+                                <Text style={{color: 'white', fontWeight: 'semibold', fontSize: 16, lineHeight: 24, marginLeft: 6}}>{current?.wind_kph}km</Text>
                             </View>
                             <View style={{flexDirection: 'row', marginLeft: 2, alignItems: 'center'}}>
                                 <Image source={require('../assets/humidity.png')} style={{height: 28, width: 28}}/>
-                                <Text style={{color: 'white', fontWeight: 'semibold', fontSize: 16, lineHeight: 24, marginLeft: 6}}>24%</Text>
+                                <Text style={{color: 'white', fontWeight: 'semibold', fontSize: 16, lineHeight: 24, marginLeft: 6}}>{current?.humidity}%</Text>
                             </View>
                             <View style={{flexDirection: 'row', marginLeft: 2, alignItems: 'center'}}>
                                 <Image source={require('../assets/alba.png')} style={{height: 28, width: 28}}/>
-                                <Text style={{color: 'white', fontWeight: 'semibold', fontSize: 16, lineHeight: 24, marginLeft: 6}}>6:05 AM</Text>
+                                <Text style={{color: 'white', fontWeight: 'semibold', fontSize: 16, lineHeight: 24, marginLeft: 6}}>{weather?.forecast?.forecastday[0]?.astro?.sunrise}</Text>
                             </View> 
                          </View>
                 </View>
@@ -103,40 +153,28 @@ export default function HomeScreen(){
                         <Text style={{color: 'white', fontSize: 16, lineHeight: 24, marginLeft: 6}}>Previsione giornaliera</Text>
                     </View>
                     <ScrollView horizontal contentContainerStyle={{paddingHorizontal: 15}} showsHorizontalScrollIndicator={false}>
-                        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: 96, borderRadius: 24, paddingBottom: 12, paddingTop: 12, marginTop: 4, marginRight: 16, backgroundColor: 'rgba(255, 255,255, 0.15)'}}>
-                            <Image source={require('../assets/heavyrain.png')} style={{height: 44, width: 44}} />
-                            <Text style={{color: 'white', marginTop: 8}}>Lunedì</Text>
-                            <Text style={{color: 'white', fontSize: 20, lineHeight: 20, fontWeight: 'semibold', marginTop: 6}}>13&#176;</Text>
-                        </View>
-                        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: 96, borderRadius: 24, paddingBottom: 12, paddingTop: 12, marginTop: 4, marginRight: 16, backgroundColor: 'rgba(255, 255,255, 0.15)'}}>
-                            <Image source={require('../assets/heavyrain.png')} style={{height: 44, width: 44}} />
-                            <Text style={{color: 'white', marginTop: 8}}>Martedì</Text>
-                            <Text style={{color: 'white', fontSize: 20, lineHeight: 20, fontWeight: 'semibold', marginTop: 6}}>13&#176;</Text>
-                        </View>
-                        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: 96, borderRadius: 24, paddingBottom: 12, paddingTop: 12, marginTop: 4, marginRight: 16, backgroundColor: 'rgba(255, 255,255, 0.15)'}}>
-                            <Image source={require('../assets/heavyrain.png')} style={{height: 44, width: 44}} />
-                            <Text style={{color: 'white', marginTop: 8}}>Mercoledì</Text>
-                            <Text style={{color: 'white', fontSize: 20, lineHeight: 20, fontWeight: 'semibold', marginTop: 6}}>13&#176;</Text>
-                        </View>
-                        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: 96, borderRadius: 24, paddingBottom: 12, paddingTop: 12, marginTop: 4, marginRight: 16, backgroundColor: 'rgba(255, 255,255, 0.15)'}}>
-                            <Image source={require('../assets/heavyrain.png')} style={{height: 44, width: 44}} />
-                            <Text style={{color: 'white', marginTop: 8}}>Giovedì</Text>
-                            <Text style={{color: 'white', fontSize: 20, lineHeight: 20, fontWeight: 'semibold', marginTop: 6}}>13&#176;</Text>
-                        </View>
-                        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: 96, borderRadius: 24, paddingBottom: 12, paddingTop: 12, marginTop: 4, marginRight: 16, backgroundColor: 'rgba(255, 255,255, 0.15)'}}>
-                            <Image source={require('../assets/heavyrain.png')} style={{height: 44, width: 44}} />
-                            <Text style={{color: 'white', marginTop: 8}}>Venerdì</Text>
-                            <Text style={{color: 'white', fontSize: 20, lineHeight: 20, fontWeight: 'semibold', marginTop: 6}}>13&#176;</Text>
-                        </View>
-                        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: 96, borderRadius: 24, paddingBottom: 12, paddingTop: 12, marginTop: 4, marginRight: 16, backgroundColor: 'rgba(255, 255,255, 0.15)'}}>
-                            <Image source={require('../assets/heavyrain.png')} style={{height: 44, width: 44}} />
-                            <Text style={{color: 'white', marginTop: 8}}>Sabato</Text>
-                            <Text style={{color: 'white', fontSize: 20, lineHeight: 20, fontWeight: 'semibold', marginTop: 6}}>13&#176;</Text>
-                        </View>
+                        {
+                            weather?.forecast?.forecastday?.map((item, index) => {
+                                let date = new Date(item.date);
+                                let options = {weekday: 'long'};
+                                let dayName = date.toLocaleDateString('it-IT', options);
+                                return(
+                                <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', width: 96, borderRadius: 24, paddingBottom: 12, paddingTop: 12, marginTop: 4, marginRight: 16, backgroundColor: 'rgba(255, 255,255, 0.15)'}} key={index}>
+                                    <Image source={weatherImages[item?.day?.condition?.text] } style={{height: 44, width: 44}} />
+                                    <Text style={{color: 'white', marginTop: 8}}>{dayName}</Text>
+                                    <Text style={{color: 'white', fontSize: 20, lineHeight: 20, fontWeight: 'semibold', marginTop: 6}}>{item?.day?.avgtemp_c}&#176;</Text>
+                                </View>
+                                )
+                            })
+                        }
+                        
                         
                     </ScrollView>
                 </View>
             </SafeAreaView>
+                )
+            }
+            
         </View>
     )
 }
